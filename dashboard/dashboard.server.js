d55bcc8ffa63a1e4bd9bd033a96c51b07fd1be57
Muhammad Shafiq <shafiq@solutionwin.com>
2025-12-02 18:09:15 +0500

    auto updated

--- dashboard/dashboard.server.js
+++ dashboard/dashboard.server.js
@@ -3,9 +3,6 @@
 import dotenv from "dotenv";
 dotenv.config({ path: ".env" });
 
-// Debug check
-console.log("Loaded ENV:", process.env);
-
 import("./build/server/index.js").catch(err => {
   console.error("Dashboard failed:", err);
   process.exit(1);
