50d885c4a9977b5b7fec1a9eb75d0f65f041d501
Muhammad Shafiq <shafiq@solutionwin.com>
2026-01-06 14:48:35 +0500

    updated

--- dashboard/app/routes/webhooks.app_subscriptions.update.tsx
+++ dashboard/app/routes/webhooks.app_subscriptions.update.tsx
@@ -2,9 +2,9 @@ import type { ActionFunctionArgs } from "react-router";
 import { authenticate } from "../shopify.server";
 import { graphqlRequest } from "../utils/graphql.client";
 import { UPDATE_SUBSCRIPTION_STATUS_MUTATION } from "app/graphql/subscriptions.mutation";
-import { createLogger } from "app/utils/logger";
+import { createModuleLogger } from "app/utils/logger";
 
-const logger = createLogger({ prefix: "webhooks.app_subscriptions" });
+const logger = createModuleLogger("webhooks.app_subscriptions");
 /**
  * Webhook handler for products/update
  * Processes product update events from Shopify
