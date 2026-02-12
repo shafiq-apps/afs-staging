3626746ea7fdf3b04392495ae7e465204451e9b2
Muhammad Shafiq <aeceshfq@gmail.com>
2026-02-12 18:40:31 +0500

    updated

---
+++ app/system/cli/zip-targz.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get current date and time in format dd-mm-HHMMSS
+datetime=$(date +"%d-%m-%H%M%S")
+
+# Use git to list files excluding .gitignore ones
+files=$(git ls-files)
+
+# Set target directory (one level up)
+target_dir="../"
+
+# Create tar.gz in the target directory
+tar -czvf "${target_dir}afsv-$datetime.tar.gz" $files
+
+echo "Created ${target_dir}afsv-$datetime.tar.gz"
