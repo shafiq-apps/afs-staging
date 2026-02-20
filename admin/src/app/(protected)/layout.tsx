8657e11e1b2f4b23fd251dd999db22d4cda62fd7
Muhammad Shafiq <aeceshfq@gmail.com>
2026-02-20 15:50:46 +0500

    updated

--- admin/src/app/(protected)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import Layout from '@/components/layout/Layout';
-
-interface ProtectedLayoutProps {
-  children: React.ReactNode;
-}
-
-export default function ProtectedLayout({ children }: ProtectedLayoutProps) {
-  return <Layout>{children}</Layout>;
-}
